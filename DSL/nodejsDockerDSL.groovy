a85cb3fc4d6884ec239223a6b3ce9ab7849a64de
juancardenas007 <159567013+juancardenas007@users.noreply.github.com>
2024-02-26 09:27:45 -0500

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/juancardenas007/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('juancardenas007')
+            node / gitConfigEmail('juan.cardenas@globant.com')
         }
     }
     triggers {
@@ -14,9 +14,9 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
+            repositoryName('davecar111/nodejsapp')
             tag('${GIT_REVISION,length=7}')
-            registryCredentials('docker-hub')
+            registryCredentials('933c49a9-c65f-4c28-b1dd-e818c5e7f7f9')
             forcePull(false)
             createFingerprints(false)
             skipDecorate()
